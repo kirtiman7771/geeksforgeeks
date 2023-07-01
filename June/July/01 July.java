75e9388d051e677070424c35ed654eed641c04ae
kirtiman7771 <kirtiman7771@gmail.com>
2023-07-01 23:48:08 +0530

    💡 POTD

---
+++ June/July/01 July.java
@@ -0,0 +1,12 @@
+// Number of 1 Bits
+class Solution {
+    static int setBits(int N) {
+        // code here
+    int count=0;
+        while(N!=0){
+            if((N&1) == 1) count++;
+            N=N>>1;
+        }
+        return count;
+    }
+}
